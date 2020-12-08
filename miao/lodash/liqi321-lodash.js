5104451f2c07413861cbaadb41f18cab05af6764
liqi <1250835405@qq.com>
2020-12-08 16:06:39 +0800

    compact

---
+++ miao/lodash/liqi321-lodash.js
@@ -0,0 +1,17 @@
+var liqi321 = {
+  chunk: function chunk(ary) {
+    var result = []
+    for (i = 0; i < ary.length; i++) {
+
+    }
+  },
+  compact: function compact(ary) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      if (ary[i]) {
+        result.push(ary[i])
+      }
+    }
+  },
+
+}
