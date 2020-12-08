e3e186d48a68a0aad6ebfa2349b0bab841c816a4
liqi <1250835405@qq.com>
2020-12-08 20:27:19 +0800

    ladash

--- miao/lodash/liqi321-lodash.js
+++ miao/lodash/liqi321-lodash.js
@@ -1,4 +1,26 @@
 var liqi321 = {
+  chunk: function chunk(ary, size) {
+    var result = []
+    var ress = []
+    var l = 0
+    if (size < 1) {
+      size = 1
+    }
+    for (i = 0; i < ary.length; i++) {
+      ress.push(ary[i])
+      l++
+      if (l == size) {
+        result.push(ress)
+        ress = []
+        l = 0
+      }
+      if (l != 0) {
+        result.push(ress)
+      }
+      return result
+    }
+  },
+
   compact: function compact(ary) {
     var result = []
     for (var i = 0; i < ary.length; i++) {
@@ -9,4 +31,21 @@ var liqi321 = {
     return result
   },
 
+  indexof: function indexof(ary, val, startIndex = 0) {
+    for (var i = startIndex; i < ary.length; i++) {
+      if (ary[i] == val) {
+        return i
+      }
+    }
+    return -1
+  },
+
+  lastindexof: function lastindexof(ary, val, startIndex = ary.length - 1) {
+    for (var i = startIndex; i >= 0; i--) {
+      if (ary[i] == val) {
+        return i
+      }
+    }
+    return -1
+  },
 }
